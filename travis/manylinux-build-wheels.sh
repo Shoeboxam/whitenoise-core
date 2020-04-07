dd2ced64ecfc8c5bac71085ea49c3e709b2f5169
Shoeboxam <shoeboxam@gmail.com>
2020-04-07 05:01:41 -0400

    travis (6)

--- travis/manylinux-build-wheels.sh
+++ travis/manylinux-build-wheels.sh
@@ -5,10 +5,11 @@
 # exit immediately upon failure, print commands while running
 set -e -x
 
+cd /io/bindings-python/
 # python3 -m pip install -r /io/bindings-python/requirements.txt
 # python3 /io/bindings-python/setup.py -d wheelhouse/
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    "${PYBIN}/pip" install -r /io/bindings-python/requirements.txt
-    "${PYBIN}/python" /io/bindings-python/setup.py bdist_wheel -d wheelhouse/
+    "${PYBIN}/pip" install -r requirements.txt
+    "${PYBIN}/python" setup.py bdist_wheel -d wheelhouse/
 done
